5babed68c671736b83627816bfe556f4630c39c4
smontode24 <sergio.mp1710@gmail.com>
2019-09-28 14:42:51 +0200

    User inherits from AbstractUser and User manager

--- piopio/piopio_be/models.py
+++ piopio/piopio_be/models.py
@@ -1,16 +1,25 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from piopio_be.managers import PiopioUserManager
+
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    first_name = models.CharField(default="Jhon ",max_length=30)
-    last_name = models.CharField(default="Ash ",max_length=150)
+    first_name = models.CharField(default="Jhon ", max_length=30)
+    last_name = models.CharField(default="Ash ", max_length=150)
+
+    def __str__(self):
+        return str(self.first_name)+" "+str(self.last_name)
 
 
-class User(models.Model):
-    username = models.CharField(default="Jhon ",max_length=30)
-    email = models.EmailField(default="user@gmail.com") 
-    password = models.CharField(max_length=50)
-    
+class User(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField(max_length=30, unique=True)
+    email = models.EmailField(unique=True)
+    profile = models.OneToOneField(Profile, on_delete=models.CASCADE)
+
+    REQUIRED_FIELDS = ['email', 'password']
+    USERNAME_FIELD = 'username'
+
+    objects = PiopioUserManager()
+
     def __str__(self):
         return self.username
