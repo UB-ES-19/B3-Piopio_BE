9c6dedd293cfa3db483f76f5bd17ec665b262d93
Cristopher Q <cristopq29@gmail.com>
2019-10-01 12:25:30 +0200

    CORS settings

--- piopio/piopio/settings.py
+++ piopio/piopio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'tb+)tx+kwa@6r=060okdz4auj1$u!v*$6n=n4pw!bmpgbp+272'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -126,3 +127,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CORS_ORIGIN_ALLOW_ALL = True
