9c634feb0e399a48e2c732cedb3ca4517051f19f
smontode24 <sergio.mp1710@gmail.com>
2019-11-17 16:20:13 +0100

    Removed unnecesary function

--- piopio/piopio_be/views.py
+++ piopio/piopio_be/views.py
@@ -172,17 +172,6 @@ class PostView(viewsets.ModelViewSet):
         serialized_posts = serializers.PostSerializerWithUser(page, many=True)
         return self.get_paginated_response(serialized_posts.data)
 
-    def list(self, request, *args, **kwargs):
-        queryset = self.filter_queryset(self.get_queryset())
-
-        page = self.paginate_queryset(queryset)
-        if page is not None:
-            serializer = self.get_serializer(page, many=True)
-            return self.get_paginated_response(serializer.data)
-
-        serializer = self.get_serializer(queryset, many=True)
-        return Response(serializer.data)
-
 
 class UserFollowerView(viewsets.GenericViewSet,
                       mixins.RetrieveModelMixin,
